181b395ffd44e106672a6aba5d993357327c4ee0
Roc Yu <rociiu.yu@gmail.com>
2021-01-18 18:26:06 +0800

    Update db/run api to support with source id and source params for generating database url

--- app/controllers/api/db_controller.rb
+++ app/controllers/api/db_controller.rb
@@ -1,7 +1,12 @@
 module Api
   class DbController < BaseController
     def run
-      conn = DbConnectionFactory.create(params[:database_url])
+      source = if params[:id]
+        DatabaseSource.find_by(uuid: params[:id])
+      else
+        DatabaseSource.new(database_source_params)
+      end
+      conn = DbConnectionFactory.create(source.generate_database_url)
       result = conn.exec_query(params[:sql])
       if conn.is_a?(ActiveRecord::ConnectionAdapters::SQLServerAdapter)
         if result.count > 0
@@ -27,6 +32,10 @@ module Api
       render json: {
         error: e.message
       }, status: :bad_request
+    end
+
+    def database_source_params
+      params.require(:source).permit(:uuid, :host, :username, :password, :port, :database, :db_type)
     end
   end
 end
--- app/models/database_source.rb
+++ app/models/database_source.rb
@@ -10,4 +10,22 @@ class DatabaseSource < ApplicationRecord
       self.encrypted_password = Rails::Secrets.encrypt(password)
     end
   end
+
+  def decrypted_password
+    if password.blank?
+      self.password = Rails::Secrets.decrypt(encrypted_password)
+    end
+  end
+
+  def generate_database_url
+    decrypted_password
+    # mysql2://root@localhost/abp_report_connector_development
+    # sqlserver://sa:StrongPassword!@localhost/abp_report_connector_development
+    case db_type
+    when 'mysql'
+      "mysql2://#{username}:#{password}@#{host}:#{port}/#{database}"
+    when 'sqlserver'
+      "sqlserver://#{username}:#{password}@#{host}:#{port}/#{database}"
+    end
+  end
 end
