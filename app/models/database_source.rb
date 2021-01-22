d5a1b48ccee8c9597b4c12a7b25c1916ad580274
Roc Yu <rociiu.yu@gmail.com>
2021-01-22 19:29:04 +0800

    Set connect_timeout to 3 in mysql

--- app/models/database_source.rb
+++ app/models/database_source.rb
@@ -23,9 +23,9 @@ class DatabaseSource < ApplicationRecord
     # sqlserver://sa:StrongPassword!@localhost/abp_report_connector_development
     case db_type
     when 'mysql'
-      "mysql2://#{username}:#{password}@#{host}:#{port}/#{database}"
+      "mysql2://#{username}:#{password}@#{host}:#{port}/#{database}?connect_timeout=3"
     when 'sqlserver'
-      "sqlserver://#{username}:#{password}@#{host}:#{port}/#{database}?login_timeout=3&timeout=10"
+      "sqlserver://#{username}:#{password}@#{host}:#{port}/#{database}?login_timeout=3&timeout=10000"
     end
   end
 end
