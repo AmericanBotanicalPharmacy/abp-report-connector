7e494a9adfd6164f64ba15f8781fbb1628d57ba2
Roc Yu <rociiu.yu@gmail.com>
2021-01-25 15:56:02 +0800

    Update db/run api to run exists db source with params for test function in edit, check database url for nil in case the db type is invalid

--- app/controllers/api/db_controller.rb
+++ app/controllers/api/db_controller.rb
@@ -6,7 +6,15 @@ module Api
       else
         DatabaseSource.new(database_source_params)
       end
-      conn = DbConnectionFactory.create(database_source.generate_database_url)
+      if database_source.persisted? && params[:source] && database_source_params.present?
+        database_source.assign_attributes(database_source_params)
+      end
+      database_url = database_source.generate_database_url
+      if database_url.blank?
+        render json: { error: 'DB not available' }, status: :bad_request
+        return
+      end
+      conn = DbConnectionFactory.create(database_url)
       result = conn.exec_query(params[:sql])
       if conn.is_a?(ActiveRecord::ConnectionAdapters::SQLServerAdapter)
         if result.count > 0
