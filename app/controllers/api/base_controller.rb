5e5d39fa241d32018da2ad904059911dff21cf29
Roc Yu <rociiu.yu@gmail.com>
2021-01-28 09:12:08 +0800

    Move TOKEN as environement variable

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,13 +1,11 @@
 class Api::BaseController < ActionController::API
-  TOKEN = "150486c440e77501633822a4b"
-
   before_action :authenticate
 
   private
 
   def authenticate
     unless request.headers['Authorization'].present? &&
-      ActiveSupport::SecurityUtils.secure_compare(request.headers['Authorization'], "Token token=#{TOKEN}")
+      ActiveSupport::SecurityUtils.secure_compare(request.headers['Authorization'], "Token token=#{ENV['TOKEN']}")
       render json: { error: 'Invalid token' }, status: :unauthorized
     end
   end
