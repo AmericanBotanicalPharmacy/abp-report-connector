d8b7be65da9f762025fee6154f2f2b6d4fb3ac71
Roc Yu <rociiu.yu@gmail.com>
2021-02-03 11:59:32 +0800

    Setup devise for user authentication, setup root url

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :sources, class_name: 'DatabaseSource'
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :omniauthable,
+         :recoverable, :rememberable, :validatable
 end
