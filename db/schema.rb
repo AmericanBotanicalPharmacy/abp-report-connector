765a204f4b11fd967b680c7cf224a946c99f635f
Roc Yu <rociiu.yu@gmail.com>
2021-03-18 19:25:58 +0800

    Setup session controller, update google sign in link

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_120600) do
+ActiveRecord::Schema.define(version: 2021_02_05_041252) do
 
   create_table "database_sources", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "db_type"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_02_04_120600) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "name"
     t.index ["user_id"], name: "index_database_sources_on_user_id"
   end
 
@@ -30,18 +31,6 @@ ActiveRecord::Schema.define(version: 2021_02_04_120600) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
