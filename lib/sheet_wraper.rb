79ff976d1c2f09d7f187990a4956f470f09ac064
Roc Yu <rociiu.yu@gmail.com>
2021-06-22 21:55:33 +0800

    Remove debug puts on the fetch_sheet_data method

--- lib/sheet_wraper.rb
+++ lib/sheet_wraper.rb
@@ -10,12 +10,7 @@ class SheetWraper
 
   def fetch_sheet_data(spreadsheet_id, range)
     response = @service.get_spreadsheet_values spreadsheet_id, range
-    puts "Name, Major:"
-    puts "No data found." if response.values.empty?
-    response.values.each do |row|
-      # Print columns A and E, which correspond to indices 0 and 4.
-      puts "#{row[0]}, #{row[4]}"
-    end
+    response.values
   end
 
   def google_secret
