919730f05021b84c1319d629411f662808088614
Roc Yu <rociiu.yu@gmail.com>
2021-07-16 14:51:01 +0800

    Add emails phones to scheduled notification

--- app/models/scheduled_notification.rb
+++ app/models/scheduled_notification.rb
@@ -4,6 +4,20 @@ class ScheduledNotification < ApplicationRecord
 
   before_destroy :remove_in_sidekiq_cron
 
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+  def emails_to_notify
+    _emails = emails || ''
+    _emails_array = _emails.split(',').map(&:strip)
+
+    _emails_array.select{ |e| e =~ EMAIL_REGEX }
+  end
+
+  def phones_to_notify
+    _phones = phones || ''
+    _phones.split(',').map(&:strip)
+  end
+
   def update_sidekiq_cron
     return if cron.blank?
     Sidekiq::Cron::Job.load_from_hash({
