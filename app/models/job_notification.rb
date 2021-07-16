24f582509843a6c036154ff1ef009598a25cb01e
Roc Yu <rociiu.yu@gmail.com>
2021-07-16 14:14:28 +0800

    Cleanup cron on JobNotification

--- app/models/job_notification.rb
+++ app/models/job_notification.rb
@@ -2,8 +2,6 @@ class JobNotification < ApplicationRecord
   belongs_to :spreadsheet_job
   belongs_to :spreadsheet
 
-  before_destroy :remove_in_sidekiq_cron
-
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
   def emails_to_notify
@@ -16,24 +14,5 @@ class JobNotification < ApplicationRecord
   def phones_to_notify
     _phones = phones || ''
     _phones.split(',').map(&:strip)
-  end
-
-  def update_sidekiq_cron
-    return if cron.blank?
-    Sidekiq::Cron::Job.load_from_hash({
-      sidekiq_cron_name => {
-        'class' => 'JobNotificationWorker',
-        'cron' => cron,
-        'args' => [id]
-      }
-    })
-  end
-
-  def remove_in_sidekiq_cron
-    Sidekiq::Cron::Job.destroy sidekiq_cron_name
-  end
-
-  def sidekiq_cron_name
-    "job-#{spreadsheet_job.id}-notification-#{id}"
   end
 end
