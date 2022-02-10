be256b8f3663f2f15605cce954530b0e537fede4
Roc Yu <rociiu0112@gmail.com>
2022-02-10 18:59:22 +0800

    Implement account page to list user spreadsheets with nav bar links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  private
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to root_path, notice: "Please Login to view that page!"
+    end
+  end
 end
