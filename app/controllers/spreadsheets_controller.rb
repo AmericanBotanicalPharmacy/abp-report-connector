342679d5b0e8775f69acb89012b82bdbac7005e0
Roc Yu <rociiu0112@gmail.com>
2022-02-28 15:27:08 +0800

    Add feature to create new spreadsheet with name and google sheet id

--- app/controllers/spreadsheets_controller.rb
+++ app/controllers/spreadsheets_controller.rb
@@ -4,4 +4,24 @@ class SpreadsheetsController < ApplicationController
   def index
     @spreadsheets = current_user.spreadsheets
   end
+
+  def new
+    @spreadsheet = Spreadsheet.new
+  end
+
+  def create
+    @spreadsheet = current_user.spreadsheets.new(spreadsheet_params)
+    if @spreadsheet.save
+      flash[:notice] = 'Successfully create spreadsheet'
+      redirect_to spreadsheets_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def spreadsheet_params
+    params.require(:spreadsheet).permit(:name, :g_id)
+  end
 end
